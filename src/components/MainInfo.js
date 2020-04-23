078ac1d9835dcd906577f09cca17e78ec9eb158c
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2020-04-23 12:50:46 +0200

    Working hook for data download, beginning building app

--- src/components/MainInfo.js
+++ src/components/MainInfo.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import useFetchedData from '../utils/useFetchedData';
 
-const MainInfo = () => (
-    <p>Main Info Component</p>
-);
+const MainInfo = () => {
+    const { data, loading, error } = useFetchedData("https://covid19.mathdro.id/api");
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>{error}</p>
+
+    return (
+        <>
+            <p>Confirmed: {data.confirmed.value}</p>
+            <p>Recovered: {data.recovered.value}</p>
+            <p>Died: {data.deaths.value}</p>
+        </>
+    )
+};
 
 export default MainInfo;
---
+++ src/utils/useFetchedData.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useFetchedData = (url) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            setLoading(true);
+            setError();
+
+            const fetchedData = await fetch(url)
+                .then(res => res.json())
+                .catch(err => setError(err));
+
+            setData(fetchedData);
+            setLoading(false);
+        }
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error};
+}
+
+export default useFetchedData;
